42fc3dbc20e820c1315bf0b51547b7201a05c18c
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2022-12-28 20:55:17 -0600

    imagen y select listos

---
+++ src/components/Image.vue
@@ -0,0 +1,39 @@
+<script>
+import { AdvancedImage } from '@cloudinary/vue';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { scale } from '@cloudinary/transformation-builder-sdk/actions/resize';
+
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'dtrkstqmm',
+  },
+});
+
+// Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+let myImg = cld.image('Coropletas_AQI/2016/Mayo2')
+
+myImg.resize(scale().height(620));
+
+
+
+export default {
+  components: {
+    AdvancedImage,
+  },
+  data() {
+    return {
+      myImg,
+    };
+  },
+};
+</script>
+
+<!-- Render the image in a Vue.js component. -->
+<template>
+  <div class="container">
+    <div class="mx-auto text-center">
+      <AdvancedImage :cldImg="myImg" />
+    </div>
+  </div>
+</template>
---
+++ src/components/Selectf.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  data() {
+    return {
+      selected1: null,
+      categorias: [
+        { text: 'Categoria', value: null, disabled: true },
+        { text: 'Coropletas AQI', value: 'aqi' },
+        { text: 'Coropletas Crimen', value: 'crimen' },
+        { text: 'HotSpots', value: 'hotspot' },
+        { text: 'Thiessen Crimen', value: 'thiessenCrimen' },
+        { text: 'Thiessen AQI', value: 'thiessenAqi' }
+      ],
+      selected2: null,
+      anios: [
+        { text: 'Año', value: null },
+        { text: '2016', value: '2016' },
+        { text: '2017', value: '2017' },
+        { text: '2018', value: '2018' },
+        { text: '2019', value: '2019' },
+        { text: '2020', value: '2020' },
+      ],
+      selected3: null,
+      meses: [
+        { text: 'Mes', value: null },
+        { text: 'Enero', value: 'Enero' },
+        { text: 'Febrero', value: 'Febrero' },
+        { text: 'Marzo', value: 'Marzo' },
+        { text: 'Abril', value: 'Abril' },
+        { text: 'Mayo', value: 'Mayo' },
+        { text: 'Junio', value: 'Junio' },
+        { text: 'Julio', value: 'Julio' },
+        { text: 'Agosto', value: 'Agosto' },
+        { text: 'Septiembre', value: 'Septiembre' },
+        { text: 'Octubre', value: 'Octubre' },
+        { text: 'Noviembre', value: 'Noviembre' },
+        { text: 'Diciembre', value: 'Diciembre' },
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+
+      <!-- Primer Select(categorias) -->
+      <div class="col-md-auto">
+        <select v-model="selected1" class="form-select " aria-label=".form-select-lg example">
+          <option v-for="categoria in categorias" :value="categoria.value">
+            {{ categoria.text }}
+          </option>
+        </select>
+
+      <!-- Segundo Select(anio) -->
+      </div>
+      <div class="col-md-auto">
+        <select v-model="selected2" class="form-select " aria-label=".form-select-lg example">
+          <option v-for="anio in anios" :value="anio.value">
+            {{ anio.text }}
+          </option>
+        </select>
+      </div>
+
+      <!-- Tercer Select(mes) si la categoria lo permite -->
+      <div class="col-md-auto" v-if="selected1 === 'aqi' || selected1 === 'crimen' || selected1 === 'hotspot' ">
+        <select v-model="selected3" class="form-select " aria-label=".form-select-lg example">
+          <option v-for="mes in meses" :value="mes.value">
+            {{ mes.text }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
